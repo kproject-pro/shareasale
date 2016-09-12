25722b168d051eba63c709a4dbf3ecfa0ec398d2
Konstantin Kiritsenko <agusteyer@gmail.com>
2016-09-11 17:40:35 -0700

    KPSAS-1 KProject GNU copyright added

--- app/code/community/KProject/ShareASale/Model/Cron.php
+++ app/code/community/KProject/ShareASale/Model/Cron.php
@@ -1,7 +1,22 @@
 <?php
 
 /**
- * @author Konstantin Kiritsenko <konstantin@kiritsenko.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see the license tag below.
+ *
+ * @author    KProject <support@kproject.pro>
+ * @license   http://www.gnu.org/licenses/ GNU General Public License, version 3
+ * @copyright 2016 KProject.pro
  */
 class KProject_ShareASale_Model_Cron
 {
--- app/code/community/KProject/ShareASale/sql/kproject_sas_setup/mysql4-install-0.1.0.php
+++ app/code/community/KProject/ShareASale/sql/kproject_sas_setup/mysql4-install-0.1.0.php
@@ -1,6 +1,22 @@
 <?php
 /**
- * @author Konstantin Kiritsenko <konstantin@kiritsenko.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see the license tag below.
+ *
+ * @author    KProject <support@kproject.pro>
+ * @license   http://www.gnu.org/licenses/ GNU General Public License, version 3
+ * @copyright 2016 KProject.pro
+ *
  * @var $installer Mage_Core_Model_Resource_Setup
  */
 $installer = $this;
@@ -26,12 +42,12 @@ $table = $installer->getConnection()
                        array(),
                        'Mage Order Increment ID'
                    )
-                    ->addColumn(
-                        'parameters',
-                        Varien_Db_Ddl_Table::TYPE_TEXT,
-                        1000,
-                        array(),
-                        'Parameters passed'
+                   ->addColumn(
+                       'parameters',
+                       Varien_Db_Ddl_Table::TYPE_TEXT,
+                       1000,
+                       array(),
+                       'Parameters passed'
                    )
                    ->addColumn(
                        'api_status',
@@ -47,15 +63,15 @@ $table = $installer->getConnection()
                        array(),
                        'API error code'
                    )
-                    ->addColumn(
-                        'retry_count',
-                        Varien_Db_Ddl_Table::TYPE_TINYINT,
-                        3,
-                        array(
-                            'default' => 0
-                        ),
-                        '# of resend attempts'
-                    );
+                   ->addColumn(
+                       'retry_count',
+                       Varien_Db_Ddl_Table::TYPE_TINYINT,
+                       3,
+                       array(
+                           'default' => 0
+                       ),
+                       '# of resend attempts'
+                   );
 
 if (!$installer->getConnection()->isTableExists($table->getName())) {
     $installer->getConnection()->createTable($table);
